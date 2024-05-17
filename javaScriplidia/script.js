0bb9559b93b879720e76457e4d9d1dd7334337b5
nikolerrz <163397767+nikolerrz@users.noreply.github.com>
2024-05-17 13:25:47 +0200

    dddd

---
+++ javaScriplidia/script.js
@@ -0,0 +1,19 @@
+//Empiece
+let nombre = "nikole";
+let edad = 27 ; 
+
+console.log("soy, " + nombre + " y tengo " + 27);
+
+// operadores matematicos//
+console.log ( 3 + 8 )
+console.log ( edad + 8)
+
+console.log ( 8 - 15 )
+console.log ( edad-15)
+
+
+console.log ( 8 * 10 )
+console.log ( edad * 15 )
+
+    console.log ( 8 / 9 )
+    console.log ( edad / 3)
