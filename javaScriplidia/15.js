c86ebabf123d00e5903e7e344551cbd61880942e
nikolerrz <163397767+nikolerrz@users.noreply.github.com>
2024-05-29 13:50:09 +0200

    29lidia

---
+++ javaScriplidia/15.js
@@ -0,0 +1,15 @@
+for (let i = 10; i > 0; i--) {
+    console.log("El valor de i es: " + i);
+}
+
+
+for (let i = 20; i > 0; i -= 2) {
+    console.log("El valor de i es  : " + i);
+}
+
+for (let i = 1; i < 1000; i*=2) {
+    console.log("El valor de i es : " + i);
+}
+
+let frase = "hola, muchacha!";
+console.log (frase.length); 
---
+++ javaScriplidia/16.js
@@ -0,0 +1,13 @@
+/*// Solicitar al usuario ingresar una frase
+let frase = prompt("Por favor, ingresa una frase:");
+
+// Calcular la cantidad de letras de la frase
+let numCaracteres = frase.length;
+
+// Imprimir en consola la cantidad de letras  de la frase
+console.log('La frase ingresada tiene ${numCaracteres} caracteres.');*/
+
+
+let palabra = prompt ( " ingresa una palabra ");
+let veces = parseInt ( prompt("ingresa el numero de veces que quieres que repita la palabra"));
+let palabraRepetida = "";
