818b1e1ef4c18c50e55175d41474f0bc09078606
nikolerrz <163397767+nikolerrz@users.noreply.github.com>
2024-06-03 12:33:39 +0200

    03lidia

---
+++ javaScriplidia/22.js
@@ -0,0 +1,23 @@
+function contarNumerosPares() {
+    let contadorPares = 0; 
+    let condicion = true;  
+
+    for (; condicion; ) {
+        let input = prompt("Ingrese un número o escriba 'parar' para detener:");
+
+        if (input.toLowerCase() === "parar") {
+            condicion = false; 
+        } else {
+            let numero = parseInt(input, 10);
+
+            if (!isNaN(numero) && numero % 2 === 0) {
+                contadorPares++; 
+            }
+        }
+    }
+
+    console.log(`Has ingresado ${contadorPares} números pares.`);
+}
+
+
+contarNumerosPares();
