abfe6f83800c8c94f221d927c91751ed1c5dfd00
nikolerrz <163397767+nikolerrz@users.noreply.github.com>
2024-06-24 14:10:51 +0200

    24 lidia

--- javaScriplidia/33.1.js
+++ javaScriplidia/33.1.js
@@ -1,14 +1,2 @@
-let opcionSeleccionada = null;
-
-function elegirOpcion(opcion) {
-    opcionSeleccionada = opcion;
-    document.getElementById("confirmacionResultado").innerText = "Has seleccionado la opción " + opcion;
-}
-
-function confirmarOpcion() {
-    if (opcionSeleccionada !== null) {
-        document.getElementById("confirmacionResultado").innerText = "Has confirmado la opción " + opcionSeleccionada;
-    } else {
-        document.getElementById("confirmacionResultado").innerText = "No has seleccionado ninguna opción para confirmar.";
-    }
-}
+let select = document.getElementById("opciones");
+let cajaInput = document.getElementById("opciones");
