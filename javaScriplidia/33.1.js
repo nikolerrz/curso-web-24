2d8fde6f740c90587f9c68fe6c2ccbf2ab2e7235
nikolerrz <163397767+nikolerrz@users.noreply.github.com>
2024-06-26 14:22:09 +0200

    26

--- javaScriplidia/33.1.js
+++ javaScriplidia/33.1.js
@@ -1,5 +1,26 @@
 let select = document.getElementById("opciones");
-let cajaInput = document.getElementById("cajatexto");
+let cajaInput = document.getElementById("cajaTexto");
 
-function actualizarTexto() { let valorOpcion = select.options [select.selectedIndex].value;
-    cajaInput.value = valorOPcion
+function actualizarTexto() {
+    let valorOpcion = select.options[select.selectedIndex].value;
+    cajaInput.value = valorOpcion;
+
+    let indiceSelec = select.selectedIndex;
+    if (indiceSelec >= 1 && indiceSelec <= 4) {  // Asegurarse de que el índice esté en el rango correcto
+        document.getElementById("boton" + indiceSelec).disabled = false;
+    }
+
+    // Deshabilitar los demás botones
+    for (let i = 1; i <= 4; i++) {
+        if (i !== indiceSelec) {
+            document.getElementById("boton" + i).disabled = true;
+        }
+    }
+}
+
+function deshabilitarBoton(indice) {
+    document.getElementById("boton" + indice).disabled = true;
+}
+
+// Añadir event listeners
+select.addEventListener('change', actualizarTexto);
---
+++ javaScriplidia/35.js
@@ -0,0 +1,6 @@
+let info = document.getElementById('info');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    info.classList.toggle('active')
+});
